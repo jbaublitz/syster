fb528e777650ff8e8a5b6dedcaeabe07462a2390
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-02-13 00:01:15 +0000

    Deploy jbaublitz/syster to github.com/jbaublitz/syster.git:gh-pages

---
+++ implementors/core/convert/trait.Into.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["syster"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.Into.html\" title=\"trait core::convert::Into\">Into</a>&lt;<a class=\"type\" href=\"syster/type.SyscallArg.html\" title=\"type syster::SyscallArg\">SyscallArg</a>&gt; for <a class=\"struct\" href=\"syster/struct.Fd.html\" title=\"struct syster::Fd\">Fd</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.Into.html\" title=\"trait core::convert::Into\">Into</a>&lt;<a class=\"type\" href=\"syster/type.SyscallArg.html\" title=\"type syster::SyscallArg\">SyscallArg</a>&gt; for <a class=\"struct\" href=\"syster/struct.Ptr.html\" title=\"struct syster::Ptr\">Ptr</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.Into.html\" title=\"trait core::convert::Into\">Into</a>&lt;<a class=\"type\" href=\"syster/type.SyscallArg.html\" title=\"type syster::SyscallArg\">SyscallArg</a>&gt; for <a class=\"struct\" href=\"syster/struct.MutPtr.html\" title=\"struct syster::MutPtr\">MutPtr</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.Into.html\" title=\"trait core::convert::Into\">Into</a>&lt;<a class=\"type\" href=\"syster/type.SyscallArg.html\" title=\"type syster::SyscallArg\">SyscallArg</a>&gt; for <a class=\"struct\" href=\"syster/struct.Int.html\" title=\"struct syster::Int\">Int</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/syster/trait.Syscall.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["syster"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
